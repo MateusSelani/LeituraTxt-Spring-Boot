ed6ceafa597dcd325f9934ceed289dd9e79fa444
Mateus Selani <selaneitor@gmail.com>
2020-07-07 11:06:58 -0300

    Alterando tipo do arquivo e adicionando replace

--- src/main/java/com/regex/regex/controllers/LendoArquivoTXT.java
+++ src/main/java/com/regex/regex/controllers/LendoArquivoTXT.java
@@ -2,7 +2,6 @@ package com.regex.regex.controllers;
 
 import java.io.IOException;
 
-import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,12 +13,20 @@ import com.regex.regex.models.RegexModels;
 @RestController
 @RequestMapping("/teste")
 public class LendoArquivoTXT {
-	
-	RegexModels arquivoModels;
-	
-	@PostMapping
-	public Resource upload(@RequestParam("texto") MultipartFile arquivoTXT) throws IOException {
 
-		return arquivoTXT.getResource();
+	RegexModels arquivoModels;
+
+	@PostMapping
+	public String upload(@RequestParam("texto") MultipartFile arquivoTXT) throws IOException {
+
+		byte[] arquivoByte = arquivoTXT.getBytes();
+
+		String arquivoString = new String(arquivoByte, "UTF-8");
+		
+		arquivoString.replace(" ", ";");
+		
+//		String coluna1 = (String) arquivoString.subSequence(0, 4);
+
+		return arquivoString;
 	}
 }

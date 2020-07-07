8c9bd533e4691f83ddab13da3a765cb75855f4a4
Mateus Selani <selaneitor@gmail.com>
2020-07-07 09:46:50 -0300

    Add Models

--- src/main/java/com/regex/regex/controllers/LendoArquivoTXT.java
+++ src/main/java/com/regex/regex/controllers/LendoArquivoTXT.java
@@ -2,21 +2,24 @@ package com.regex.regex.controllers;
 
 import java.io.IOException;
 
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.regex.regex.models.RegexModels;
+
 @RestController
 @RequestMapping("/teste")
 public class LendoArquivoTXT {
-
-	MultipartFile arquivoTXT;
-
+	
+	RegexModels arquivoModels;
+	
 	@PostMapping
-	public byte[] upload(@RequestParam("texto") MultipartFile arquivoTXT) throws IOException {
+	public Resource upload(@RequestParam("texto") MultipartFile arquivoTXT) throws IOException {
 
-		return arquivoTXT.getBytes();
+		return arquivoTXT.getResource();
 	}
 }
---
+++ src/main/java/com/regex/regex/models/RegexModels.java
@@ -0,0 +1,24 @@
+package com.regex.regex.models;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@Setter(value = AccessLevel.PACKAGE)
+@Getter
+public class RegexModels {
+
+	private long id_garrafa;
+	
+	private String maquina;
+	
+	private String descricao;
+	
+//	private Date data;
+}
